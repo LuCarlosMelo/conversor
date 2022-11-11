b5a4e6b5365a9c25c8e08a580990c7fffb1f81ab
LuCarlosMelo <Luizcapeme@gmail.com>
2022-11-10 21:40:15 -0300

    Realizado lógica para encerrar o programa caso seja clicado no x

--- src/Program.java
+++ src/Program.java
@@ -1,29 +1,57 @@
-import java.awt.Frame;
-
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import model.entities.Coin;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Object [] converterOptions = {"Moeda", "Temperatura"}; 
-		Object chosenConverter = JOptionPane.showInputDialog(null, "O que você deseja converter?", "Menu",
-				JOptionPane.INFORMATION_MESSAGE ,null, converterOptions,converterOptions[0]);
+		Double escolha;
+
 		
-		Object [] coinOption = {"De Reais a Dólares", "De Reais a Euros","De Reais a Libras","De Reais a Yenes",
-				"De Reais a Won Coreano","De Dólares a Reais ", "De Euros a Reais ", "De Libras a Reais",
-				"De Yenes a Reais ","De Won Coreano a Reais "};
-		
-		if(chosenConverter == converterOptions[0]) {
-			JOptionPane.showInputDialog(null, "Escolha a moeda para qual deseja converter:","Menu",JOptionPane.INFORMATION_MESSAGE,null, coinOption,coinOption[0]);
+		do {
+		JFrame frame = new JFrame();
+
+		Object[] converterOptions = { "Moeda", "Temperatura" };
+
+		JOptionPane.showInputDialog(null, "O que você deseja converter?", "Menu", JOptionPane.INFORMATION_MESSAGE, null,
+				converterOptions, converterOptions[0]);
+
+		Object[] coinOption = { "De Reais a Dolares", "De Reais a Euros", "De Reais a Libras", "De Reais a Yenes",
+				"De Reais a Won Coreano", "De Dólares a Reais ", "De Euros a Reais ", "De Libras a Reais",
+				"De Yenes a Reais ", "De Won Coreano a Reais " };
+		Object value = JOptionPane.showInputDialog(null, "Escolha a moeda para qual deseja converter:", "Menu",
+				JOptionPane.INFORMATION_MESSAGE, null, coinOption, coinOption[0]);
+		try {
+			Object objInputValue = JOptionPane.showInputDialog("Insira um valor: ");
+			String strInputValue = objInputValue.toString();
+			Double inputValue = Double.valueOf(strInputValue).doubleValue();
+
+			Object objResult = new Coin().converterCoin(value, inputValue);
+			String result = objResult.toString();
+
+			JOptionPane.showMessageDialog(frame, "O valor " + value + " é : " + result, "Resultado",
+					JOptionPane.INFORMATION_MESSAGE);
+		}
+
+		catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(frame, "Valor inválido", "Message", JOptionPane.ERROR_MESSAGE);
 		}
 		
-		Object inputValue = JOptionPane.showInputDialog("Insira um valor: ");
-		
-		JFrame frame = new JFrame("Alow");
-		
-		Object outValue = JOptionPane.showMessageDialog(frame, "Puts");
-		System.exit(0);
-	}
+		Object objEscolha = JOptionPane.showConfirmDialog(null, "Escolha um: ", "Escolha um", JOptionPane.YES_NO_CANCEL_OPTION);
+		String strEscolha = objEscolha.toString();
+		Double doubleEscolha = Double.valueOf(strEscolha).doubleValue();
+		escolha = doubleEscolha;
+		}
+		while(escolha == 0);
+	
+		if(escolha == 1 || escolha == -1) {
+			JOptionPane.showMessageDialog(null, "Programa finalizado");
+		} else {
+			JOptionPane.showMessageDialog(null, "Programa concluído");
 
+		}
+		
+	
+	
+
+}
 }
