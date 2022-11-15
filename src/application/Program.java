b4259334091569bc90e51a3eddd1f69b78915dc6
LuCarlosMelo <Luizcapeme@gmail.com>
2022-11-15 08:25:00 -0300

    Classe Program integrada ao package application

---
+++ src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import javax.swing.*;
+import model.entities.Coin;
+
+public class Program {
+	public static void main(String[] args) {
+
+		Double escolha;
+
+		do {
+			JFrame frame = new JFrame();
+
+			Object[] converterOptions = { "Moeda", "Temperatura" };
+
+			JOptionPane.showInputDialog(null, "O que você deseja converter?", "Menu", JOptionPane.INFORMATION_MESSAGE,
+					null, converterOptions, converterOptions[0]);
+
+			Object[] coinOption = { "De Reais a Dolares", "De Reais a Euros", "De Reais a Libras", "De Reais a Yenes",
+					"De Reais a Won Coreano", "De Dólares a Reais ", "De Euros a Reais ", "De Libras a Reais",
+					"De Yenes a Reais ", "De Won Coreano a Reais " };
+			Object value = JOptionPane.showInputDialog(null, "Escolha a moeda para qual deseja converter:", "Moedas",
+					JOptionPane.INFORMATION_MESSAGE, null, coinOption, coinOption[0]);
+			try {
+				Object objInputValue = JOptionPane.showInputDialog("Insira um valor: ");
+				String strInputValue = objInputValue.toString();
+				Double inputValue = Double.valueOf(strInputValue).doubleValue();
+
+				Object objResult = new Coin().converterCoin(value, inputValue);
+				String result = objResult.toString();
+
+				JOptionPane.showMessageDialog(frame, "O valor da conversão é de: R$ " + result, "Resultado",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+
+			catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(frame, "Valor inválido", "Message", JOptionPane.ERROR_MESSAGE);
+			}
+
+			Object objEscolha = JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Selecione uma opção",
+					JOptionPane.YES_NO_CANCEL_OPTION);
+			String strEscolha = objEscolha.toString();
+			Double doubleEscolha = Double.valueOf(strEscolha).doubleValue();
+			escolha = doubleEscolha;
+		} while (escolha == 0);
+
+		if (escolha == 1 || escolha == -1) {
+			JOptionPane.showMessageDialog(null, "Programa finalizado");
+		} else {
+			JOptionPane.showMessageDialog(null, "Programa concluído");
+
+		}
+
+	}
+}
